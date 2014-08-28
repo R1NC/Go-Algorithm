5026d2c03566a34d672da8ca4aacabfeac16a4f4
Rinc <i@RincLiu.com>
2014-08-28 00:24:32 -0400

    Modified FlagBubbleSort.

--- algorithms/sort/bubble-sort.go
+++ algorithms/sort/bubble-sort.go
@@ -16,56 +16,50 @@ func swap(array []int, index1 int, index2 int) {
 	array[index2] = tmp
 }
 
-func NaiveBubbleSort(array []int) int {
+func NaiveBubbleSort(array []int) {
 	checkArray(array)
-	var compare int
 	for i := 0; i < len(array); i++ {
 		for j := 0; j < len(array) - 1; j++ {
 			if array[j] > array[j + 1] {
 				swap(array, j, j + 1)
 			}
-			compare++
 		}
 	}
-	return compare
 }
 
-func FlagBubbleSort(array []int) int {
+func FlagBubbleSort(array []int) {
 	checkArray(array)
-	var compare int
-	var is_last_scan_swapped bool
+	var has_swapped bool
 	for i := 0; i < len(array); i++ {
-		is_swapped = false
+		has_swapped = false
 		for j := 0; j < len(array) - 1; j++ {
 			if array[j] > array[j + 1] {
 				swap(array, j, j + 1)
-				is_swapped = true
+				has_swapped = true
 			}
-			compare++
 		}
-		// if last scan has no swapping, the array is sorted.
+		// if last scan has not swapped, the array is sorted.
 		// Then there's no need to scan again.
-		if !is_swapped {
+		if !has_swapped {
 			break
 		}
 	}
-	return compare
 }
 
 func main() {
-	array0 := []int{21, 5, 342, 55, 9, 17, 105}
-	array1 := array0[:]
-	array2 := array0[:]
-	//array3 := array0[:]
+	array1 := []int{1, 3, 2, 5, 4}
+	array2 := []int{1, 3, 2, 5, 4}
 	fmt.Println("Initial array:")
-	for _, x0 := range array0 {
+	for _, x0 := range array1 {
 		fmt.Printf("%d\n", x0)
 	}
-	fmt.Printf("NaiveBubbleSorted with compare: %d\n", NaiveBubbleSort(array1))
+	fmt.Println("NaiveBubbleSort...")
+	NaiveBubbleSort(array1)
 	for _, x1 := range array1 {
 		fmt.Printf("%d\n", x1)
 	}
-	fmt.Printf("FlagBubbleSorted with compare: %d\n", FlagBubbleSort(array2))
+	fmt.Println("FlagBubbleSort...")
+	FlagBubbleSort(array2)
 	for _, x2 := range array2 {
 		fmt.Printf("%d\n", x2)
 	}
