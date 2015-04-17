8be70455a0e5d54669ad1cfa5f63890c601fb9e3
RincLiu <i@RincLiu.com>
2015-04-17 03:11:56 -0700

    KMP

--- string/single-pattern-search.go
+++ string/single-pattern-search.go
@@ -7,7 +7,44 @@ import (
 */
 
 func KMPSearch(source string, pattern string) int {
-	return -1
+	si := 0
+	pi := 0
+	nextPatternIndexArray := generateNextPatternIndexArray(pattern)
+	for si < len(source) && pi < len(pattern) {
+		if pi == -1 || ([]byte(source)[si] == []byte(pattern)[pi]) {
+			si++
+			pi++
+		} else {
+			pi = nextPatternIndexArray[pi]
+		}
+	}
+	if pi == len(pattern) {
+		return si - pi
+	} else {
+		return -1
+	}
+}
+
+func generateNextPatternIndexArray(pattern string) []int {
+	length := len(pattern)
+	array := make([]int, length)
+	array[0] = -1
+	prefixI := -1
+	suffixI := 0
+	for suffixI < length - 1 {
+		if prefixI == -1 || ([]byte(pattern)[prefixI] == []byte(pattern)[suffixI]) {
+			prefixI++
+			suffixI++
+			if []byte(pattern)[prefixI] != []byte(pattern)[suffixI] {
+				array[suffixI] = prefixI
+			} else {
+				array[suffixI] = array[prefixI]
+			}
+		} else {
+			prefixI = array[prefixI]
+		}
+	}
+	return array
 }
 
 func BMSearch(source string, pattern string) int {
